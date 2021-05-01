679e3d32b2748c8b312cb8e2e6db6f42fe74024c
Julien Hagestedt <julien.hagestedt@gmail.com>
2021-05-01 09:22:20 +0200

    feat: openapi and gh cli test (#4)

--- src/test/java/com/example/OpenApiTest.java
+++ src/test/java/com/example/OpenApiTest.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.net.URL;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 
@@ -13,9 +14,12 @@ import org.springframework.test.context.ContextConfiguration;
 @ContextConfiguration(classes = App.class)
 public class OpenApiTest {
 
+  @Value("${springdoc.api-docs.path}")
+  private String path;
+
   @Test
   public void json() throws IOException {
-    try (BufferedInputStream in = new BufferedInputStream(new URL("http://localhost:8080/openapi").openStream());
+    try (BufferedInputStream in = new BufferedInputStream(new URL(String.format("http://localhost:8080%s", path)).openStream());
          FileOutputStream fileOutputStream = new FileOutputStream("target/openapi.json")) {
       byte[] dataBuffer = new byte[1024];
       int bytesRead;
